b48400bddf3ffdf0e46cfd6bfe366ea1f36d9431
Huzaifa Khan <muhammadhuzaifakhan277@gmail.com>
2022-01-05 15:59:50 +0500

    basic structure has been set up

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).colorScheme.secondary
+            ],
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              "Connecting Hearts '❤️' ",
+              style: TextStyle(
+                fontFamily: 'DancingScript',
+                fontSize: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
